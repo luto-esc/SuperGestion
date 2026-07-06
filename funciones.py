fd0ad9100686c41117634eaeb7586bd5fd159708
AgusMore <agustinmoreyra0414@gmail.com>
2026-07-06 00:27:52 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -41,7 +41,7 @@ def StraInt(caracter):
 	elif caracter == '6':
 		caracter = 6
 	elif caracter == '7':
-   carácter = 7
+        carácter = 7
 	elif caracter == '0':
 		caracter = 0
 	return caracter
